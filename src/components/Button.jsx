e24b15444724a24567bb6d02ef20a2c043700854
asadcodex <aslanrazasr@gmail.com>
2025-03-09 18:57:49 +0530

    Initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+const Button = ({ name, isBeam = false, containerClass }) => {
+  const handleClick = () => {
+    const element = document.getElementById('contact');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <button className={`btn ${containerClass}`} onClick={handleClick}>
+      {isBeam && (
+        <span className="relative flex h-3 w-3">
+          <span className="btn-ping"></span>
+          <span className="btn-ping_dot"></span>
+        </span>
+      )}
+      {name}
+    </button>
+  );
+};
+
+export default Button;
